5a48f4091e1602e6bd4bf0991bc02412f0b12976
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-07 22:06:11 +0300

    Create CategoryEntityConfiguration.cs

---
+++ FinalCourseProject/BookFindingAndRatingSystem.Data/Configurations/CategoryEntityConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using BookFindingAndRatingSystem.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.Collections.Generic;
+
+namespace BookFindingAndRatingSystem.Data.Configurations
+{
+    public class CategoryEntityConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasData(this.GenereteCategories());
+        }
+        private Category[] GenereteCategories()
+        {
+            ICollection<Category> categories = new HashSet<Category>();
+            Category category;
+            category = new Category() { Id = 1, Name = "Horror" };
+            categories.Add(category);
+            category = new Category() { Id = 2, Name = "Romance" };
+            categories.Add(category);
+            category = new Category() { Id = 3, Name = "Science Fiction" };
+            categories.Add(category);
+            category = new Category() { Id = 4, Name = "Mystery" };
+            categories.Add(category);
+            category = new Category() { Id = 5, Name = "Fantasy" };
+            categories.Add(category);
+            category = new Category() { Id = 6, Name = "Thriller" };
+            categories.Add(category);
+            category = new Category() { Id = 7, Name = "Comedy" };
+            categories.Add(category);
+            category = new Category() { Id = 8, Name = "Fiction" };
+            categories.Add(category);
+            return categories.ToArray();
+        }
+    }
+}
