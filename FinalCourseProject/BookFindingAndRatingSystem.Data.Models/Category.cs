a7883ad4662fd3258445da973599047e3e7f0b9a
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-08 10:34:36 +0300

    Create Category.cs

---
+++ FinalCourseProject/BookFindingAndRatingSystem.Data.Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static BookFindingAndRatingSystem.Common.EntityValidationConstants.GenreConst;
+namespace BookFindingAndRatingSystem.Data.Models
+{
+    public class Category
+    {
+        public Category()
+        {
+            this.Books = new HashSet<Book>();
+        }   
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(GenreNameMaxLength)]
+        public string Name { get; set; } = null!;
+
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
