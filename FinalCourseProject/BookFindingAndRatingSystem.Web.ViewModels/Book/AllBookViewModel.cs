7abe0f0637888f21d241ae187cc0b60772f08fd2
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-09 10:26:56 +0300

    Summary

---
+++ FinalCourseProject/BookFindingAndRatingSystem.Web.ViewModels/Book/AllBookViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace BookFindingAndRatingSystem.Web.ViewModels.Book
+{
+    public class AllBookViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string Title { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public short Pages { get; set; }
+
+        public string ImageUrl { get; set; } = null!;
+
+        public float Price { get; set; }
+
+    }
+}
