68a3b5308a83a32dba7d19c65bc6b555ee9a2e9b
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-10 14:38:56 +0300

    Create Autor.cs

---
+++ FinalCourseProject/BookFindingAndRatingSystem.Data.Models/Autor.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static BookFindingAndRatingSystem.Common.EntityValidationConstants.AutorConst;
+namespace BookFindingAndRatingSystem.Data.Models
+{
+    public class Autor
+    {
+        public Autor()
+        {
+            this.Books = new List<Book>();
+        }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(FirstNameMaxLength)]
+        public string FirstName { get; set; } = null!;
+        [Required]
+        [MaxLength(LastNameMaxLength)]
+        public string LastName { get; set; } = null!;
+        [Required]
+        public string ImageUrl { get; set; } = null!;
+        [Required]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime BirthDate { get; set; }
+        public ICollection<Book> Books { get; set; }
+    }
+}
