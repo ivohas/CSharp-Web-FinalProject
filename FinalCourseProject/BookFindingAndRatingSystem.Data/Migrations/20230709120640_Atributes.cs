8ebdebe85b731e1ab997ac6de6438068faae36a6
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-12 23:21:58 +0300

    Create 20230709120640_Atributes.cs

---
+++ FinalCourseProject/BookFindingAndRatingSystem.Data/Migrations/20230709120640_Atributes.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookFindingAndRatingSystem.Data.Migrations
+{
+    public partial class Atributes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("365606b6-91cc-462f-b38d-e384d7bdd8ce"));
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("ac5e9819-9906-4edc-830d-483e9b00d91d"));
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("bb3bab06-8989-4f32-8307-f8a55a371d43"));
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("dde66f05-9f52-46e1-a13d-f4891aaf8da0"));
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("e1a9a6a8-3e75-4825-97cc-8bd2af168738"));
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("ef0b5a0b-8467-4085-89fa-a68f3496f4ce"));
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AplicationUserId", "AutorId", "CategoryId", "Description", "ImageUrl", "Pages", "Price", "PublishersId", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("106a5bda-2514-4c3e-929a-aa655dee1e12"), null, 4, 5, "A fantasy novel by J.R.R. Tolkien", "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1546071216l/5907.jpg", (short)304, 11.99f, 5, "The Hobbit" },
+                    { new Guid("531b4a80-d8d4-4fca-b9dc-94aa42735d7c"), null, 3, 8, "A novel by F. Scott Fitzgerald", "https://www.hachettebookgroup.com/wp-content/uploads/2020/06/9780762498147-3.jpg?fit=572%2C750", (short)180, 7.99f, 4, "The Great Gatsby" },
+                    { new Guid("84207d11-ea79-4182-b7a1-3f8e7dcff4db"), null, 6, 8, "A classic novel by Harper Lee", "https://upload.wikimedia.org/wikipedia/commons/4/4f/To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg", (short)336, 12.99f, 1, "To Kill a Mockingbird" },
+                    { new Guid("9ffb239e-184f-404b-917b-5204672f0242"), null, 2, 8, "A coming-of-age novel by J.D. Salinger", "https://m.media-amazon.com/images/I/61fgOuZfBGL._AC_UF1000,1000_QL80_.jpg", (short)224, 8.99f, 3, "The Catcher in the Rye" },
+                    { new Guid("a2419829-11fb-4c1d-802e-af5e002ca577"), null, 1, 8, "A dystopian novel by George Orwell", "https://knigomania.bg/media/catalog/product/cache/02f16ac392ba7c312a70e2f3c5d752a7/1/9/1984-9780451524935.jpg", (short)328, 9.99f, 2, "1984" },
+                    { new Guid("ce12aa63-7289-4748-902d-2e513550718b"), null, 5, 5, "The first book in the Harry Potter series by J.K. Rowling", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0Ro_Cc4bCs0AH3X_1UfgeuIB6PhU6j-sN6A&usqp=CAU", (short)332, 10.99f, 6, "Harry Potter and the Philosopher's Stone" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("106a5bda-2514-4c3e-929a-aa655dee1e12"));
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("531b4a80-d8d4-4fca-b9dc-94aa42735d7c"));
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("84207d11-ea79-4182-b7a1-3f8e7dcff4db"));
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("9ffb239e-184f-404b-917b-5204672f0242"));
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("a2419829-11fb-4c1d-802e-af5e002ca577"));
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "Id",
+                keyValue: new Guid("ce12aa63-7289-4748-902d-2e513550718b"));
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "Id", "AplicationUserId", "AutorId", "CategoryId", "Description", "ImageUrl", "Pages", "Price", "PublishersId", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("365606b6-91cc-462f-b38d-e384d7bdd8ce"), null, 6, 8, "A classic novel by Harper Lee", "https://upload.wikimedia.org/wikipedia/commons/4/4f/To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg", (short)336, 12.99f, 1, "To Kill a Mockingbird" },
+                    { new Guid("ac5e9819-9906-4edc-830d-483e9b00d91d"), null, 5, 5, "The first book in the Harry Potter series by J.K. Rowling", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0Ro_Cc4bCs0AH3X_1UfgeuIB6PhU6j-sN6A&usqp=CAU", (short)332, 10.99f, 6, "Harry Potter and the Philosopher's Stone" },
+                    { new Guid("bb3bab06-8989-4f32-8307-f8a55a371d43"), null, 3, 8, "A novel by F. Scott Fitzgerald", "https://www.hachettebookgroup.com/wp-content/uploads/2020/06/9780762498147-3.jpg?fit=572%2C750", (short)180, 7.99f, 4, "The Great Gatsby" },
+                    { new Guid("dde66f05-9f52-46e1-a13d-f4891aaf8da0"), null, 1, 8, "A dystopian novel by George Orwell", "https://knigomania.bg/media/catalog/product/cache/02f16ac392ba7c312a70e2f3c5d752a7/1/9/1984-9780451524935.jpg", (short)328, 9.99f, 2, "1984" },
+                    { new Guid("e1a9a6a8-3e75-4825-97cc-8bd2af168738"), null, 4, 5, "A fantasy novel by J.R.R. Tolkien", "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1546071216l/5907.jpg", (short)304, 11.99f, 5, "The Hobbit" },
+                    { new Guid("ef0b5a0b-8467-4085-89fa-a68f3496f4ce"), null, 2, 8, "A coming-of-age novel by J.D. Salinger", "https://m.media-amazon.com/images/I/61fgOuZfBGL._AC_UF1000,1000_QL80_.jpg", (short)224, 8.99f, 3, "The Catcher in the Rye" }
+                });
+        }
+    }
+}
