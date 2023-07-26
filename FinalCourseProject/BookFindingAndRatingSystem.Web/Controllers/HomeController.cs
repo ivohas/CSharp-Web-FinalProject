0c73b0f053cb59e17838b3043da95ca1ed3b4517
ivohas <105879092+ivohas@users.noreply.github.com>
2023-07-26 16:48:29 +0300

    New Home Page

--- FinalCourseProject/BookFindingAndRatingSystem.Web/Controllers/HomeController.cs
+++ FinalCourseProject/BookFindingAndRatingSystem.Web/Controllers/HomeController.cs
@@ -1,32 +1,48 @@
-﻿using BookFindingAndRatingSystem.Web.ViewModels.Home;
+﻿using BookFindingAndRatingSystem.Services.Data.Interfaces;
+using BookFindingAndRatingSystem.Web.ViewModels.Book;
 using Library.Controllers;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
 
 namespace BookFindingAndRatingSystem.Web.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : BaseController
     {
-        
+        private readonly IBookService bookService;
 
-        public HomeController()
-        {            
+        public HomeController(IBookService bookService)
+        {
+            this.bookService = bookService;
         }
         [HttpGet]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            IEnumerable<AllBookViewModel> books;
+            try
+            {
+                books = await this.bookService.GetBooksByNumberOfSellsAsync();
+            }
+            catch (Exception)
+            {
+                return this.BadRequest("Something went wrong");
+
+            }
+
+            books = books.Take(2).ToArray();
+
+            return View(books);
         }
-        
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error(int statusCode)
         {
-            if (statusCode==400||statusCode==404)
+            if (statusCode == 400 || statusCode == 404)
             {
                 return this.View("Error404");
             }
-            if (statusCode==401)
+            if (statusCode == 401)
             {
                 return this.View("Error401");
             }
