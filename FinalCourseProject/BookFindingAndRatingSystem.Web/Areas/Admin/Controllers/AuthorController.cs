e577a366ab4d7faea09029aad8e8deaeaa6f99a0
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-06 20:11:49 +0300

    Move add author to admin area

---
+++ FinalCourseProject/BookFindingAndRatingSystem.Web/Areas/Admin/Controllers/AuthorController.cs
@@ -0,0 +1,46 @@
+﻿using BookFindingAndRatingSystem.Services.Data.Interfaces;
+using BookFindingAndRatingSystem.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static BookFindingAndRatingSystem.Common.GeneralApplicationConstansts;
+namespace BookFindingAndRatingSystem.Web.Areas.Admin.Controllers
+{
+    public class AuthorController : BaseAdminController
+    {
+        private IAuthorService authorService;
+        public AuthorController(IAuthorService authorService)
+        {
+            this.authorService = authorService;
+        }
+
+        [HttpGet]
+        [Authorize(Roles = AdminRoleName)]
+        public IActionResult Add()
+        {
+            var model = new AuthorViewModel();
+            return View(model);
+        }
+        [HttpPost]
+        [Authorize(Roles = AdminRoleName)]
+        public async Task<IActionResult> Add(AuthorViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("Model", "Invalid data");
+                return this.View(model);
+            }
+            try
+            {
+                await this.authorService.CreateNewAuthorAsync(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest("Couldn't save the new auhtor");
+            }
+
+
+            return RedirectToAction("All", "Author", new { area = "" });
+
+        }
+    }
+}
--- FinalCourseProject/BookFindingAndRatingSystem.Web/Controllers/AuthorController.cs
+++ FinalCourseProject/BookFindingAndRatingSystem.Web/Controllers/AuthorController.cs
@@ -42,35 +42,7 @@ namespace BookFindingAndRatingSystem.Web.Controllers
             }
 
             return View(author);
-        }
-        [HttpGet]
-        [Authorize(Roles = AdminRoleName)]
-        public IActionResult Add()
-        {
-            var model = new AuthorViewModel();
-            return View(model);
-        }
-        [HttpPost]
-        [Authorize(Roles = AdminRoleName)]
-        public async Task<IActionResult> Add(AuthorViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                ModelState.AddModelError("Model", "Invalid data");
-                return this.View(model);
-            }
-            try
-            {
-                await this.authorService.CreateNewAuthorAsync(model);
-            }
-            catch (Exception)
-            {
-                return BadRequest("Couldn't save the new auhtor");
-            }
-
-
-            return RedirectToAction(nameof(All));
-        }
+        }       
         [HttpGet]
         [Authorize(Roles = AdminRoleName)]
         public async Task<IActionResult> Edit(int id)
