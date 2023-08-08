5282a9997ba1e1880084716fb4634845049d8343
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-08 11:45:12 +0300

    Logger and exception handling in the user service

--- FinalCourseProject/BookFindingAndRatingSystem.Services.Data/UserService.cs
+++ FinalCourseProject/BookFindingAndRatingSystem.Services.Data/UserService.cs
@@ -1,129 +1,172 @@
-﻿using BookFindingAndRatingSystem.Data.Models;
-using BookFindingAndRatingSystem.Services.Data.Interfaces;
+﻿using BookFindingAndRatingSystem.Services.Data.Interfaces;
 using BookFindingAndRatingSystem.ViewModels;
 using BookFindingAndRatingSystem.Web.Data;
 using BookFindingAndRatingSystem.Web.ViewModels.User;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace BookFindingAndRatingSystem.Services.Data
 {
     public class UserService : IUserService
     {
-        private readonly BooksDbContext dbContext;
+        private readonly ILogger<UserService> _logger;
+        private readonly BooksDbContext _dbContext;
 
-        public UserService(BooksDbContext dbContext)
+        public UserService(ILogger<UserService> logger, BooksDbContext dbContext)
         {
-            this.dbContext = dbContext;
+            _logger = logger;
+            _dbContext = dbContext;
         }
 
         public async Task AddOrEditAboutForUserByIdAsync(string? userId, ProfileViewModel model)
         {
-            var user = await this.dbContext.Users.Where(x => x.Id.ToString() == userId).FirstOrDefaultAsync();
-
-            if (user != null)
+            try
             {
-                user.About = model.About;
-                await dbContext.SaveChangesAsync();
-            }
-            else
-            {
-                throw new Exception("Problem occured in saving the info about you!");
-            }
+                var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id.ToString() == userId);
 
+                if (user != null)
+                {
+                    user.About = model.About;
+                    await _dbContext.SaveChangesAsync();
+                }
+                else
+                {
+                    throw new Exception("Problem occurred while saving the info about you!");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while saving About info for user.");
+                throw; // Rethrow the exception
+            }
         }
 
         public async Task AddOrEditReadingChallengeAsync(string? userId, ProfileViewModel model)
         {
-            var user = await this.dbContext.Users.Where(x => x.Id.ToString() == userId).FirstOrDefaultAsync();
+            try
+            {
+                var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id.ToString() == userId);
 
-            if (user != null)
-            {
-                user.ReadingChallenge = model.ReadingChalenge;
-                await dbContext.SaveChangesAsync();
+                if (user != null)
+                {
+                    user.ReadingChallenge = model.ReadingChalenge;
+                    await _dbContext.SaveChangesAsync();
+                }
+                else
+                {
+                    throw new Exception("Problem occurred while saving the info about you!");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                throw new Exception("Problem occured in saving the info about you!");
+                _logger.LogError(ex, "An error occurred while saving Reading Challenge for user.");
+                throw; // Rethrow the exception
             }
         }
 
         public async Task<IEnumerable<UserViewModel>> AllAsync()
         {
-            IEnumerable<UserViewModel> users; 
             try
             {
-                 users = await this.dbContext
-                            .Users
-                            .Select(u => new UserViewModel
-                            {
-                                Email = u.Email,
-                                PhoneNumber = u.PhoneNumber,
-                                UserName = u.UserName
+                IEnumerable<UserViewModel> users = await _dbContext.Users
+                    .Select(u => new UserViewModel
+                    {
+                        Email = u.Email,
+                        PhoneNumber = u.PhoneNumber,
+                        UserName = u.UserName
+                    })
+                    .ToArrayAsync();
 
-                            }).ToArrayAsync();
+                return users;
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                throw;
+                _logger.LogError(ex, "An error occurred while fetching all users.");
+                throw; // Rethrow the exception
             }
-
-            return users;
         }
 
         public async Task ChangeImageUrl(string userId, ProfileViewModel model)
         {
-            var user = await this.dbContext.Users.Where(x => x.Id.ToString() == userId).FirstOrDefaultAsync();
+            try
+            {
+                var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id.ToString() == userId);
 
-            if (user != null)
-            {
-                user.ImageUrl = model.ImageUrl;
-                await dbContext.SaveChangesAsync();
+                if (user != null)
+                {
+                    user.ImageUrl = model.ImageUrl;
+                    await _dbContext.SaveChangesAsync();
+                }
+                else
+                {
+                    throw new Exception("Problem occurred while saving the info about you!");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                throw new Exception("Problem occured in saving the info about you!");
+                _logger.LogError(ex, "An error occurred while changing Image URL for user.");
+                throw; // Rethrow the exception
             }
         }
 
         public async Task EditUserNameAsync(string userId, ProfileViewModel model)
         {
-            bool userNameAlreadyExist = await this.dbContext.Users.Where(u => u.UserName == model.UserName).AnyAsync();
-
-            if (userNameAlreadyExist)
+            try
             {
-                throw new ArgumentException("There is already user with this username!");
-            }
+                bool userNameAlreadyExist = await _dbContext.Users
+                    .Where(u => u.UserName == model.UserName)
+                    .AnyAsync();
 
-            var user = await this.dbContext.Users.Where(x => x.Id.ToString() == userId).FirstOrDefaultAsync();
-            if (user == null)
+                if (userNameAlreadyExist)
+                {
+                    throw new ArgumentException("There is already a user with this username!");
+                }
+
+                var user = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id.ToString() == userId);
+                if (user == null)
+                {
+                    throw new Exception("Problem occurred.");
+                }
+
+                user.UserName = model.UserName;
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
             {
-                throw new Exception("Problem occured");
+                _logger.LogError(ex, "An error occurred while editing username for user.");
+                throw; // Rethrow the exception
             }
-
-            user.UserName = model.UserName;
-            await dbContext.SaveChangesAsync();
         }
 
         public async Task<ProfileViewModel> GetInfoByIdAsync(string? userId)
         {
-            var info = await this.dbContext.Users.Where(x => x.Id.ToString() == userId).FirstOrDefaultAsync();
-
-            if (info != null)
+            try
             {
-                var model = new ProfileViewModel()
-                {
-                    Id = info.Id,
-                    Email = info.Email,
-                    PhoneNumber = info.PhoneNumber,
-                    UserName = info.UserName,
-                    About = info.About,
-                    ReadingChalenge = info.ReadingChallenge,
-                    ImageUrl = info.ImageUrl
-                };
+                var info = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id.ToString() == userId);
 
-                return model;
+                if (info != null)
+                {
+                    var model = new ProfileViewModel()
+                    {
+                        Id = info.Id,
+                        Email = info.Email,
+                        PhoneNumber = info.PhoneNumber,
+                        UserName = info.UserName,
+                        About = info.About,
+                        ReadingChalenge = info.ReadingChallenge,
+                        ImageUrl = info.ImageUrl
+                    };
+
+                    return model;
+                }
+
+                throw new ArgumentException("There isn't any user with this id");
             }
-            throw new ArgumentException("There isn't any user with this id");
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while getting user info by id.");
+                throw; // Rethrow the exception
+            }
         }
     }
 }
