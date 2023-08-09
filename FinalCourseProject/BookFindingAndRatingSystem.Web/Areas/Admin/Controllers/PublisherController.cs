7e7aaa509727b9b06dd932d6369602a16a37a09a
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-09 14:23:54 +0300

    Add tempData and errro handing to publisher controller

--- FinalCourseProject/BookFindingAndRatingSystem.Web/Areas/Admin/Controllers/PublisherController.cs
+++ FinalCourseProject/BookFindingAndRatingSystem.Web/Areas/Admin/Controllers/PublisherController.cs
@@ -3,15 +3,18 @@ using BookFindingAndRatingSystem.Web.ViewModels.Publisher;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using static BookFindingAndRatingSystem.Common.GeneralApplicationConstansts;
+
 namespace BookFindingAndRatingSystem.Web.Areas.Admin.Controllers
 {
     public class PublisherController : BaseAdminController
     {
         private readonly IPublisherService publisherService;
+
         public PublisherController(IPublisherService publisherService)
         {
             this.publisherService = publisherService;
         }
+
         [HttpGet]
         [Authorize(Roles = AdminRoleName)]
         public IActionResult Add()
@@ -19,22 +22,27 @@ namespace BookFindingAndRatingSystem.Web.Areas.Admin.Controllers
             var model = new AddPublisherViewModel();
             return View(model);
         }
+
         [HttpPost]
         [Authorize(Roles = AdminRoleName)]
         public async Task<IActionResult> Add(AddPublisherViewModel model)
         {
             if (!ModelState.IsValid)
             {
+                TempData["ErrorMessage"] = "Invalid data!";
                 ModelState.AddModelError("Model", "Invalid data");
-                return this.View(model);
+                return View(model);
             }
+
             try
             {
                 await this.publisherService.CreateNewPublisher(model);
+                TempData["SuccessMessage"] = "New publisher added successfully!";
             }
             catch (Exception)
             {
-                return BadRequest("Couldn't save the new publisher");
+                TempData["ErrorMessage"] = "Couldn't save the new publisher due to an error!";
+                return RedirectToAction(nameof(Add));
             }
 
             return RedirectToAction("Index", "Home");
