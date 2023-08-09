b57dec9f38b14c78d9341878f0751a4cf4e67731
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-09 14:18:57 +0300

    Add logger and error handing in publisher service

--- FinalCourseProject/BookFindingAndRatingSystem.Services.Data/PublisherService.cs
+++ FinalCourseProject/BookFindingAndRatingSystem.Services.Data/PublisherService.cs
@@ -1,35 +1,68 @@
-﻿using BookFindingAndRatingSystem.Data.Models;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using BookFindingAndRatingSystem.Data.Models;
 using BookFindingAndRatingSystem.Services.Data.Interfaces;
 using BookFindingAndRatingSystem.Web.Data;
 using BookFindingAndRatingSystem.Web.ViewModels.Publisher;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace BookFindingAndRatingSystem.Services.Data
 {
     public class PublisherService : IPublisherService
     {
+        private readonly ILogger<PublisherService> _logger;
+        private readonly BooksDbContext _dbContext;
 
-        private readonly BooksDbContext dbContext;
-
-        public PublisherService(BooksDbContext dbContext)
+        public PublisherService(ILogger<PublisherService> logger, BooksDbContext dbContext)
         {
-            this.dbContext = dbContext;
+            _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        private async Task<Publisher> GetPublisherByNameAsync(string name)
+        {
+            return await _dbContext.Publishers.FirstOrDefaultAsync(p => p.Name.ToLower() == name.ToLower());
+        }
+
+        private async Task SaveChangesAsync()
+        {
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while saving changes to the database.");
+                throw; // Rethrow the exception
+            }
         }
 
         public async Task CreateNewPublisher(AddPublisherViewModel model)
         {
-            bool alreadyExist = await this.dbContext.Publishers.Where(p => p.Name.ToLower() == model.Name.ToLower()).AnyAsync();
-            if (alreadyExist)
+            try
             {
-                throw new Exception("There is already publisher with this name");
-            }
-            var publisher = new Publisher()
-            {
-                Name = model.Name
-            };
+                var existingPublisher = await GetPublisherByNameAsync(model.Name);
 
-            this.dbContext.Publishers.AddRange(publisher);
-            await this.dbContext.SaveChangesAsync();
+                if (existingPublisher != null)
+                {
+                    throw new Exception("A publisher with this name already exists.");
+                }
+
+                var newPublisher = new Publisher()
+                {
+                    Name = model.Name
+                };
+
+                _dbContext.Publishers.Add(newPublisher);
+                await SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while creating a new publisher.");
+                throw; // Rethrow the exception
+            }
         }
     }
 }
