23495ec3222a8970349b47828366c26c59bb7a6c
ivohas <105879092+ivohas@users.noreply.github.com>
2023-08-10 11:23:27 +0300

    Logger and error handing in category service

--- FinalCourseProject/BookFindingAndRatingSystem.Services.Data/CategoryService.cs
+++ FinalCourseProject/BookFindingAndRatingSystem.Services.Data/CategoryService.cs
@@ -1,23 +1,40 @@
 ﻿using BookFindingAndRatingSystem.Services.Data.Interfaces;
 using BookFindingAndRatingSystem.Web.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BookFindingAndRatingSystem.Services.Data
 {
     public class CategoryService : ICategoryService
     {
-        private readonly BooksDbContext dbContext;
+        private readonly ILogger<CategoryService> _logger;
+        private readonly BooksDbContext _dbContext;
 
-        public CategoryService(BooksDbContext dbContext)
+        public CategoryService(ILogger<CategoryService> logger, BooksDbContext dbContext)
         {
-            this.dbContext = dbContext;
+            _logger = logger;
+            _dbContext = dbContext;
         }
 
         public async Task<IEnumerable<string>> AllCategoriesNameAsync()
         {
-            return await this.dbContext.Categories
-                .Select(c => c.Name)
-                .ToArrayAsync();
+            try
+            {
+                var categoryNames = await _dbContext.Categories
+                    .Select(c => c.Name)
+                    .ToArrayAsync();
+
+                return categoryNames;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while fetching all category names.");
+                throw; // Rethrow the exception
+            }
         }
     }
 }
